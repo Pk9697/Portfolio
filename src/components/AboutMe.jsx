94bde0eb6af720692965100d05ba703193f4b346
Prashant Kumar <prashant.aman9@gmail.com>
2022-12-13 22:05:30 +0530

    styling titles and subtitles

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,7 +4,7 @@ function AboutMe() {
   return (
     <section className='about-me' id='about'>
         <h2 className="section__title section__title--about">Who I am</h2>
-        <p className='section__subtitle section__subtitle--about'></p>
+        <p className='section__subtitle section__subtitle--about'>Designer & developer based out of NYC</p>
         <div className="about-me__body">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna
